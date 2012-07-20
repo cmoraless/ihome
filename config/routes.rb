d28dd096be930d778442b9ce34140aba32cfc45d
ptarud <paulo.tarud@gmail.com>
2012-07-20 12:35:25 -0400

    creando interfaz super admin

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Ihome::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :houses
 
   # Sample resource route with options:
   #   resources :products do
