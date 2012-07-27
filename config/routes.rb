6c5143f30d573b4cb90f4bc53f9afd3df74d979a
ptarud <paulo.tarud@gmail.com>
2012-07-27 16:38:35 -0400

    controlado basicamente

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Ihome::Application.routes.draw do
     resources :accessories
   end
 
-  resources :accessories
+  #resources :accessories 
 
   resources :accessory_types
 
@@ -72,12 +72,11 @@ Ihome::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 	root :to => 'start#index'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-	match ':controller(/:action(/:id))(.:format)'
-
+	 match ':controller(/:action(/:id))(.:format)'
+  match 'accessories/control' => 'accessories/control'
 	#resources :house
 end
