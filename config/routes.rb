793085c172bc6aca0397ff91d55fa4a945d615c3
ptarud <paulo.tarud@gmail.com>
2012-07-30 15:41:31 -0400

    arreglado

--- config/routes.rb
+++ config/routes.rb
@@ -77,6 +77,5 @@ Ihome::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
 	 match ':controller(/:action(/:id))(.:format)'
-  match 'accessories/control' => 'accessories/control'
 	#resources :house
 end
