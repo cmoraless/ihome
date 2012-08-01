9126d4bce275c9351aa8e94502c307987ad6e752
cmoraless <cristian@inspiron1420.(none)>
2012-08-01 15:25:26 -0400

    correcionqlia

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,14 +6,6 @@ class AdminController < ApplicationController
   end
   
   def view
-<<<<<<< HEAD
-    respond_to do |format|
-      format.html { } # show.html.erb
-      format.json  { render :json => @house }
-    end
-=======
-    
->>>>>>> 40a53e2ffcda23487e5220208c9d3976727a380e
   end
     
 end
