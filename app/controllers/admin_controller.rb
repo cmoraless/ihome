17d66049b18d74d4d54099989d918c1c4c66b933
ptarud <paulo.tarud@gmail.com>
2012-08-03 12:03:02 -0400

    arreglada vista home a bootstrap

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,8 @@
 class AdminController < ApplicationController
   def index
+    @accessories = Accessory.all
     @user = User.find(session[:user_id])
-    @iboxes = @user.iboxes.all
+    @iboxes = @user.iboxes
     
   end
   
