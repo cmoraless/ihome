f823f4bcce51b1bb6d7b293391807a0b45eec3c3
ptarud <paulo.tarud@gmail.com>
2012-08-17 11:31:18 -0400

    camara probando2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,6 +22,23 @@ class HomeController < ApplicationController
       session[:ibox_id] = @ibox.id
       @containers = IboxAccessoriesContainer.where("ibox_id = ?", @ibox.id)
       @cameras = @ibox.cameras
+      #Consumo servicio de camaras para autentificarme
+      require 'net/http'
+      require 'uri'  
+      for i in 0..@cameras.length-1
+        ws = 'http://' + @cameras[i][:ip] + ':' + @cameras[i][:port]
+        uri = URI.parse(ws)
+        begin
+          http = Net::HTTP.new(uri.host, uri.port)
+          request = Net::HTTP::Get.new(uri.request_uri)
+          request.basic_auth(@cameras[i][:user], @cameras[i][:password])
+          response = http.request(request)
+          logger.debug "#########RESPONSEEE #{response}"
+        rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+          Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError,SocketError => e
+        end
+      end
+      #logger.debug "########### CAMERAS #{@cameras}"
     else
       flash[:notice] = "Debe habilitar su Ibox en Administracion"
     end
--- app/controllers/homeadmin_controller.rb
+++ app/controllers/homeadmin_controller.rb
@@ -1,7 +1,19 @@
 class HomeadminController < ApplicationController
   def index
     @iboxes = Ibox.all
-    @accessory_types = AccessoryType.all;
+    @accessory_types = AccessoryType.all
+    #logger.debug "####################ACCESSORY TYPES   #{@accessory_types.length}"
+    if @accessory_types.length == 0
+      @accessory = AccessoryType.new(:name=>"Cortinas")
+      @accessory.save
+      @accessory = AccessoryType.new(:name=>'Luces')
+      @accessory.save
+      @accessory = AccessoryType.new(:name=>'Dimmers')
+      @accessory.save
+      @accessory = AccessoryType.new(:name=>'Sensores')
+      @accessory.save      
+    end
+    @accessory_types = AccessoryType.all
     @users = User.all
     respond_to do |format|
       format.html  # index.html.erb
