35d87d98650b1b6e56c08f897ca2dee259a61feb
cmoraless <cristian@inspiron1420.(none)>
2012-08-17 15:27:56 -0400

    iconos en listings

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,7 +1,7 @@
 class Accessory < ActiveRecord::Base
   # asociaciones:
   belongs_to :accessory_type
-  has_and_belongs_to_many :profile_accessories_containers
+  #has_and_belongs_to_many :profile_accessories_containers
   has_and_belongs_to_many :ibox_accessories_containers
   # atributos
   attr_accessible :name, :zid, :kind, :value, :cmdclass, :state, :isScheduled
