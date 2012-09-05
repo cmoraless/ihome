8eeb2571079b6ee51367178f9ae3f255ac054ead
ptarud <paulo.tarud@gmail.com>
2012-09-04 22:34:41 -0300

    cosas esteticas en homeadmin y cambiado labels IP a DNS

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -2,7 +2,7 @@ class Camera < ActiveRecord::Base
   belongs_to :ibox
   attr_accessible :ip, :name, :password, :port, :user
   #validaciones
-  validates :ip, :presence => {:message=> "^La IP no puede estar en blanco."}, :format=> {:with => /^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}$/, :message=>"^La IP no es valida."}
+  validates :ip, :presence => {:message=> "^El DNS o IP no puede estar en blanco."}#, :format=> {:with => /^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}$/, :message=>"^La IP no es valida."}
   validates :port, :presence => {:message=> "^El puerto no puede estar en blanco."}, :format=> {:with => /^\d+$/, :message=>"^El puerto debe ser numerico."}
   validates :user, :presence => {:message => "^El usuario no puede estar en blanco."}
   
