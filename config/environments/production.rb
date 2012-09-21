f0c50a59894f417e48ee3820aaa88d829b277547
ptarud <paulo.tarud@gmail.com>
2012-09-20 21:54:47 -0300

    configuracion action mailer en production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ Ihome::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'ihome.bissen',
+    :password             => 'b1ss3n.1h0m3',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
