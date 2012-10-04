051673906a22218a954d7f82d8d01f059cfed9fb
cmoraless <cristian@inspiron1420.(none)>
2012-10-04 16:43:01 -0300

    webservices como el ibox

--- app/controllers/accessories_controller.rb
+++ app/controllers/accessories_controller.rb
@@ -69,7 +69,7 @@ class AccessoriesController < ApplicationController
       if @accessory.update_attributes(params[:accessory])
         #guardo el nombre del accesorio en el ibox
         name = @accessory.name #lo asigno para borrarle los espacios para pasarselo al webservice
-        iboxExecute(@ibox.ip, @ibox.port, '/cgi-bin/Set.cgi?ZID=' + @accessory.zid + '&ALIAS=' + name.delete(' ') + '&X=' + @accessory.x  + '&Y=' + @accessory.y + '&W=' + @accessory.w + '&H=' + @accessory.h + '&Layer=0',@ibox.user,@ibox.password)        
+        #iboxExecute(@ibox.ip, @ibox.port, '/cgi-bin/Set.cgi?ZID=' + @accessory.zid + '&ALIAS=' + name.delete(' ') + '&X=' + @accessory.x  + '&Y=' + @accessory.y + '&W=' + @accessory.w + '&H=' + @accessory.h + '&Layer=0',@ibox.user,@ibox.password)        
         # se agrega al nuevo containter
         if (!@ibox.accessory_types.find_by_name(@accessory.accessory_type.name))
           @ibox.accessory_types << @accessory.accessory_type
@@ -131,7 +131,8 @@ class AccessoriesController < ApplicationController
             req = Net::HTTP::Get.new(url.path + '/cgi-bin/Switch.cgi?&ZID=' + accessory.zid + '&VALUE=' + params[:value] + '&DEALY=&TIMER=')
           end
           if accessory.kind == 'BinarySwitch'
-            req = Net::HTTP::Get.new(url.path + '/cgi-bin/Switch.cgi?OP=' + params[:value] + '&ZID=' + accessory.zid)
+            #req = Net::HTTP::Get.new(url.path + '/cgi-bin/Switch.cgi?OP=' + params[:value] + '&ZID=' + accessory.zid)
+            req = Net::HTTP::Get.new(url.path + '/cgi-bin/Switch.cgi?&ZID=' + accessory.zid + '&OP='   + params[:value] + '&DEALY=&TIMER=')
           end
           req.basic_auth ibox.user, ibox.password
           res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
--- app/controllers/iboxes_controller.rb
+++ app/controllers/iboxes_controller.rb
@@ -440,7 +440,7 @@ class IboxesController < ApplicationController
         
         #guardo el nombre del accesorio en el ibox
         name = @accessory.name #borro los espacios para pasarselo al webservice
-        iboxExecute(@ibox.ip, @ibox.port, '/cgi-bin/Set.cgi?ZID=' + @accessory.zid + '&ALIAS=' + name.delete(' ') + '&X=' + @accessory.x  + '&Y=' + @accessory.y + '&W=' + @accessory.w + '&H=' + @accessory.h + '&Layer=0',@ibox.user,@ibox.password)
+        #iboxExecute(@ibox.ip, @ibox.port, '/cgi-bin/Set.cgi?ZID=' + @accessory.zid + '&ALIAS=' + name.delete(' ') + '&X=' + @accessory.x  + '&Y=' + @accessory.y + '&W=' + @accessory.w + '&H=' + @accessory.h + '&Layer=0',@ibox.user,@ibox.password)
         #Se busca el contenedor del Ibox y tipo
         @container = IboxAccessoriesContainer.find_by_ibox_id_and_accessory_type_id(@ibox.id, @accessory_type.id)
         #se le cambia el nombre al contenedor
